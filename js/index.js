713e76e698abf65675687accb149548ab7316f51
Tim Whitacre <tim@timw.co>
2016-02-29 00:26:52 -0500

    added js and updated styles

---
+++ js/index.js
@@ -0,0 +1,44 @@
+(function() {
+  'use strict';
+
+  buildDisplay(0, 6000);
+
+  function createProgress (percent) {
+    var bar         = document.createElement('span');
+    var pop         = document.createElement('span');
+
+    pop.className   = 'percentage';
+    pop.innerText   = percent + '%';
+
+    bar.className   = 'meter';
+    bar.style.width = percent + '%';
+    bar.appendChild(pop);
+
+    return bar;
+  }
+
+  function updateTotal (raised, goal) {
+    var raisedArea = document.querySelector('#raised');
+    var goalArea = document.querySelector('#goal');
+
+    raisedArea.innerText = numberWithCommas(raised);
+    goalArea.innerText = numberWithCommas(goal);
+  }
+
+  function buildDisplay (raised, goal) {
+    var progress  = document.querySelector('#progress'),
+        percent   = calculatePercent(raised, goal),
+        bar       = createProgress(percent);
+    updateTotal(raised, goal);
+    progress.appendChild(bar);
+  }
+
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  function calculatePercent (raised, goal) {
+    return Math.floor((raised / goal) * 100);
+  }
+
+}());
