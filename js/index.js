7e133ecfffe11c56eb2028ca0e0e5e24bdcac297
Tim Whitacre <tim@timw.co>
2016-04-24 18:32:43 -0400

    updated total

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  buildDisplay(3500, 6000);
+  buildDisplay(3550, 6000);
 
   function createProgress (percent) {
     var bar         = document.createElement('span');
